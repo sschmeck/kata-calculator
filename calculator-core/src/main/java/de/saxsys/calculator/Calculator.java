8e1bbec43ebe3db6f24881e068fed16bb26e8a09
Sebastian Schmeck <sebastian.schmeck@googlemail.com>
2012-02-26 18:22:32 +0100

    Add calculator dummy class

---
+++ calculator-core/src/main/java/de/saxsys/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package de.saxsys.calculator;
+
+public class Calculator {
+
+	/**
+	 * @param button
+	 * @return number to display
+	 */
+	public String press(String button) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- calculator-core/src/main/java/de/saxsys/calculator/CalculatorApplication.java
+++ calculator-core/src/main/java/de/saxsys/calculator/CalculatorApplication.java
@@ -5,7 +5,14 @@ public class CalculatorApplication {
 	public static void main(String[] args) {
 		
 		if (args.length > 0) {
-			// TODO
+
+			String result = "";
+			Calculator calculator = new Calculator();
+			char[] chars = args[0].toCharArray();
+			for (int i = 0; i < chars.length; i++) {
+				result = calculator.press(String.valueOf(chars[i]));				
+			}
+			System.out.println(result);
 		}
 	}
 }
